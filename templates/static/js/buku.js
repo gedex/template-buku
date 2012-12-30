c96dc5a963345921afefec16778f26ed9d82f6b1
Akeda Bagus <akeda@x-team.com>
2012-12-30 13:57:00 +0700

    Remove unused static assets and new style for new template's look.

--- templates/static/js/buku.js
+++ templates/static/js/buku.js
@@ -4,50 +4,5 @@
 
 	var $window = $(window);
 
-	// Disable certain links in docs
-  $('section [href^=#]').click(function (e) {
-    e.preventDefault()
-  })
-
-	$('#hide-me').click(function() {
-			$('#sidebar').addClass('hide');
-			$('#book').addClass('full');
-			$('#show-me-nav').addClass('shown');
-
-			pinnedToggleNav();
-	});
-
-	$('#show-me-nav').click(function() {
-		$('#book').removeClass('full');
-		$('#sidebar').removeClass('hide');
-		$('#show-me-nav').removeClass('shown');
-	});
-
-	$('#chapter-nav').affix({
-		offset: {
-			top: 10,
-			bottom: 270
-		}
-	});
-
-	$window.scroll(function() {
-		pinnedToggleNav();
-	});
-
-	function pinnedToggleNav() {
-		if ( $('#show-me-nav').hasClass('shown') ) {
-			var winTop = $(window).scrollTop() - 1;
-
-			$('#show-me-nav').css({top: winTop});
-		}
-
-		if ( $(window).width() > 767 ) {
-			if ( $('#chapter-nav').hasClass('affix') || $('#chapter-nav').hasClass('affix-bottom') ) {
-				$('#hide-me').css({top: 22});
-			} else {
-				$('#hide-me').css({top: 32});
-			}
-		}
-	}
 
 }(window.jQuery);
